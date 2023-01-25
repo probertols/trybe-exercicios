e012c8abaa6974122849b6e90d9e9c039e33d224
Paulo Roberto <paulomd32@gmail.com>
2023-01-25 00:29:42 -0300

    exercicios de javascript 1

---
+++ 03-intro-javascript/01-exercicios/script.js
@@ -0,0 +1,59 @@
+/* Exercicio 1
+
+const num1 = 3;
+const num2 = 2;
+
+if (num1 > num2){
+    console.log("Esse número é o maior:" + num1);
+}
+else {
+    console.log("Esse número é o maior:" + num2); 
+}
+
+___________________________________________________________________
+
+Exercicio 2 
+
+const num1 = 10;
+const num2 = 10;
+const num3 = 10;
+
+if(num1 > num2 && num1 > num3){
+    console.log("Esse número é o maior:" + num1); 
+}
+else if(num2 > num1 && num2 > num3){
+    console.log("Esse número é o maior:" + num2); 
+}
+
+else if(num3 > num1 && num3 > num2){
+    console.log("Esse número é o maior:" + num3); 
+}
+
+else {
+    console.log("Condição de empate ou não prevista");
+}
+______________________________________________________
+
+Exercicio 3
+
+const num = 4;
+
+if(num > 0){
+    console.log("Positive");
+}
+
+else if(num < 0){
+    console.log("Negative");
+}
+
+else{
+    console.log("Valor é igual a 0");
+}
+
+*/
+
+const a = 5;
+const b = 12;
+const c = 2;
+
+
